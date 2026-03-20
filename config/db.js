233fcb64c24ffb229fc154d9af517a625fa01ff4
Sadra S <73902811+sadra17@users.noreply.github.com>
2026-03-20 12:48:15 -0400

    Refactor MongoDB error logging message

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
-
 const connect = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error("MongoDB connection error:", err.message);
+    console.error("MongoDB error:", err.message);
     process.exit(1);
   }
 };
-
 module.exports = connect;
