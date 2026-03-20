281d60f2552673d7dabf4ce7fb272eaace201850
Sadra S <73902811+sadra17@users.noreply.github.com>
2026-03-20 12:48:29 -0400

    Update slots.js

--- config/slots.js
+++ config/slots.js
@@ -1,14 +1,9 @@
 const ALL_SLOTS = [
-  "9:00 AM",  "9:40 AM",
-  "10:00 AM", "10:40 AM",
-  "11:00 AM", "11:40 AM",
-  "12:00 PM",
-  "1:00 PM",  "1:40 PM",
-  "2:00 PM",  "2:40 PM",
-  "3:00 PM",  "3:40 PM",
-  "4:00 PM",
+  "9:00 AM","9:40 AM","10:00 AM","10:40 AM",
+  "11:00 AM","11:40 AM","12:00 PM",
+  "1:00 PM","1:40 PM","2:00 PM",
+  "2:40 PM","3:00 PM","3:40 PM","4:00 PM",
 ];
-
 const toMinutes = (slot) => {
   const [time, period] = slot.split(" ");
   let [h, m] = time.split(":").map(Number);
@@ -16,5 +11,4 @@ const toMinutes = (slot) => {
   if (period === "AM" && h === 12) h = 0;
   return h * 60 + m;
 };
-
 module.exports = { ALL_SLOTS, toMinutes };
