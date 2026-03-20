e476bef751ced25804a35cea2d11a814ead323f7
Sadra S <73902811+sadra17@users.noreply.github.com>
2026-03-19 21:34:45 -0400

    Simplify MongoDB connection options

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      serverSelectionTimeoutMS: 5000,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
